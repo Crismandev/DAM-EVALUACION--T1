52a21261b38939e6d300c6ae4085789d2f95cd16
Crismandev <crisman.dev@gmail.com>
2026-09-23 01:37:00 -0500

    Fix: Quitar findViewByID(R.id.main) roto en Pregunta 3 tras estandarización de UI

--- appgrupo11/app/src/main/java/pe/edu/cibertec/appgrupo11/Pregunta3Activity.kt
+++ appgrupo11/app/src/main/java/pe/edu/cibertec/appgrupo11/Pregunta3Activity.kt
@@ -20,12 +20,7 @@ class Pregunta3Activity : AppCompatActivity(), View.OnClickListener {
         binding = ActivityPregunta3Binding.inflate(layoutInflater)
 
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
+        
        binding.btnCalcular.setOnClickListener(this)
     }
 
