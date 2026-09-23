4cce019919fec020169bc8190b9dda8cdf33afbe
LuisCs <luiscarlos.pv28@gmail.com>
2026-09-23 00:57:03 -0500

    Agrego Pregunta 4 - Sobregiro temporal

--- appgrupo11/app/src/main/java/pe/edu/cibertec/appgrupo11/MainActivity.kt
+++ appgrupo11/app/src/main/java/pe/edu/cibertec/appgrupo11/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -21,6 +22,10 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.btnPregunta1).setOnClickListener {
             startActivity(Intent(this, Pregunta1Activity::class.java))
+        }
+
+        findViewById<Button>(R.id.btnPregunta4).setOnClickListener {
+            startActivity(Intent(this, Pregunta4Activity::class.java))
         }
     }
 }
