e63a6fe7473385b4ea210022485b54ee09cac571
Crismandev <crisman.dev@gmail.com>
2026-09-23 01:17:06 -0500

    Feat: Integración de Pregunta 5 desde commit e9e6ef7

--- appgrupo11/app/src/main/java/pe/edu/cibertec/appgrupo11/MainActivity.kt
+++ appgrupo11/app/src/main/java/pe/edu/cibertec/appgrupo11/MainActivity.kt
@@ -51,6 +51,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, Pregunta4Activity::class.java))
         }
 
+        findViewById<Button>(R.id.btnPregunta5).setOnClickListener {
+            startActivity(Intent(this, Pregunta5Activity::class.java))
+        }
+
         findViewById<Button>(R.id.btnPregunta6).setOnClickListener {
             startActivity(Intent(this, Pregunta6Activity::class.java))
         }
