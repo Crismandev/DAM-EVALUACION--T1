880095c4fe31f5bab09d680b34986a5bbbfdc1a1
am4teratsu <sub.amateratsu@gmail.com>
2026-09-22 21:28:30 -0500

    feat: agregar libreria Glide y habilitar View Binding

--- appgrupo11/app/build.gradle.kts
+++ appgrupo11/app/build.gradle.kts
@@ -29,6 +29,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    viewBinding {
+        enable=true
+    }
 }
 
 dependencies {
@@ -37,6 +41,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+    implementation("com.github.bumptech.glide:glide:5.0.9")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
